880731bbea4d398428bb50a11b32cc89447f11bd
hcche <hcche@plateer.com>
2025-03-26 16:58:38 +0900

    데이터 베이스 설정

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -6,12 +6,12 @@ const prisma = new PrismaClient();
 async function truncateAllTable() {
   const tablenames = await prisma.$queryRaw<
     Array<{ tablename: string }>
-  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+  >`SELECT tablename FROM pg_tables WHERE schemaname='mn_mgr'`;
 
   const tables = tablenames
     .map(({ tablename }) => tablename)
     .filter((name) => name !== '_prisma_migrations')
-    .map((name) => `"public"."${name}"`)
+    .map((name) => `"mn_mgr"."${name}"`)
     .join(', ');
 
   try {
